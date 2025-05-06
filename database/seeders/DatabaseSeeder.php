cf0cf66b44a6fc905d74d9c4ca5530269fa70daf
h3y-retr0 <jgr.espiga@protonmail.com>
2025-05-06 22:27:41 +0200

    Seed database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,7 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $testUser = User::factory()->hasContacts(30)->createOne(['email' => 'test@test.com']);
+
+        $users = User::factory(3)->hasContacts(5)->create()->each(
+            fn ($user) => $user->contacts->first()->sharedWithUsers()->attach($testUser->id)
+        );
+
+        $testUser->contacts->first()->sharedWithUsers()->attach($users->pluck('id'));
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
